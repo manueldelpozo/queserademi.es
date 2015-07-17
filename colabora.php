61498316c50230e453088bd346a9927b28ea5120
manuel <mdp.webdeveloper@gmail.com>
2015-07-17 17:17:53 +0200

    cambios en el grid y orden de codigo

--- colabora.php
+++ colabora.php
@@ -47,14 +47,18 @@
     height="0" width="0" style="display:none;visibility:hidden"></iframe>  
     </noscript>
     <script>
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        (function(w,d,s,l,i){
+          w[l]=w[l]||[];
+          w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-5MQKZX');
     </script>
     <script type="text/javascript">
       $('#ex1').slider({
+        alert("funciona");
         formatter: function(value) {
           return 'Current value: ' + value;
         }
@@ -86,7 +90,7 @@
           <div class="row body">
 
             <div class="col-md-6 col-xs-12 text-center">
-              <div class="col-md-8 col-offset-2 col-xs-12">
+              <div class="col-md-8 col-md-offset-2 col-xs-12">
                 <input name="nombre" type="text" id="nombre" class="typeahead principal center-block form-control input-lg" autocomplete="off" placeholder="Tu nombre completo" value="<?php //echo @$profesion_uno; ?>" autofocus>
                 <input name="email" type="email" id="email" class="typeahead principal center-block form-control input-lg" autocomplete="off" placeholder="Tu direccion email" value="<?php //echo @$profesion_uno; ?>">
                 <div class="dropdown clearfix profesion">
@@ -112,7 +116,7 @@
             </div>
 
             <div class="col-md-6 col-xs-12 text-center">
-              <div class="col-md-8 col-offset-2 col-xs-12">
+              <div class="col-md-8 col-md-offset-2 col-xs-12">
 
                 <div class="col-md-12 col-xs-12 text-center">
                   <h2>SALARIO APROXIMADO</h2>
