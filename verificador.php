47e8118dbdb4c73ebb64deb6a4882f4b1a868a5a
manuel <mdp.webdeveloper@gmail.com>
2015-07-18 17:19:34 +0200

    verificacion email y dominio

--- verificador.php
+++ verificador.php
@@ -4,5 +4,21 @@ if ($_POST['verificacion'] != ""){
     exit();
 }else{
     // Es un usuario real, proceder a enviar el formulario.
+
+
+	// VALIDAR EMAIL??
+	$email = $_POST['email'];
+	$domail = substr( $email, strpos($email,'@') );
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    	// invalid emailaddress
+    	$error =+ 0.3;
+		//Additionally you can check whether the domain defines an MX record:
+		if (!checkdnsrr($domain, 'MX')) {
+		    // domain is not valid
+		    $error =+ 0.2;
+		}
+	}
+
+	
 }
 ?>
