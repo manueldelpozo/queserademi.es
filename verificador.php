8d74bebfc9f66be90ee049c945291435e4957339
manuel <mdp.webdeveloper@gmail.com>
2015-07-18 18:51:14 +0200

    limpieza de codigo

--- verificador.php
+++ verificador.php
@@ -1,37 +1,31 @@
 <?php
-if ($_POST['verificacion'] != ""){
+
+if( !empty( $_POST['verificacion'] ) ){
     // Es un SPAMbot
     exit();
-}else{
-    // Es un usuario real, proceder a enviar el formulario.
-
+} else {
 	$error;
 
 	// VALIDAR EMAIL??
 	$email = $_POST['email'];
 	$domain = substr( $email, strpos($email,'@') );
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    	// invalid emailaddress
+	// invalid emailaddress
+    if ( !filter_var( $email, FILTER_VALIDATE_EMAIL ) )
     	$error =+ 0.1;
-	}
 	//Additionally you can check whether the domain defines an MX record:
-	if (!checkdnsrr($domain, 'MX')) {
-	    // domain is not valid
-	    $error =+ 0.1;
-	}
-
+	if ( !checkdnsrr( $domain, 'MX' ) )
+		$error =+ 0.1;
+	
 	//MEDIR CONCORDANCIA DE CONTENIDOS
 	$profesion = $_POST['profesion'];
 	$estudios = $_POST['estuios_asoc'];
 	$descripcion = $_POST['descripcion'];
 	if( !empty($descripcion) ) {
 		if( !empty($estudios) ) {
-			if( !preg_match("/($profesion|$estudios)/i", $descripcion) ) {
-				//none found
-				$error =+ 0.1;
-			}
+			if( !preg_match("/($profesion|$estudios)/i", $descripcion) )
+				$error =+ 0.1; 
 		} else {
-			if( !preg_match("/$profesion/i", $descripcion) ) {
+			if( !preg_match("/$profesion/i", $descripcion) )
 				$error =+ 0.05;
 		}
 	}
