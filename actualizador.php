bfc039bdd1dc724a87ee3ed628dff989a1b76a35
manuel <mdp.webdeveloper@gmail.com>
2015-07-19 17:30:59 +0200

    pseudocodigo para el actualizador

---
+++ actualizador.php
@@ -0,0 +1,17 @@
+<?php
+
+// Consultamos valores de la tabla colaboraciones
+// Recorremos valores en filas
+
+	// Comprobar aceptacion. Si aceptado es true procedemos
+
+	// Si existe ya la profesion 
+	// consultamos los valores guardados
+	// generar una media aritmetica con los nuevos valores NUMERICOS
+	// agregar estudios asociados y descripcion si no existen
+	// chequear descripciones alternativas??
+	// guardar valores antiguos y nuevos en otro registro?
+
+	// insertar los nuevos datos en la tabla profesiones
+
+?>
