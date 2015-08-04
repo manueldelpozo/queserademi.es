717c4cc70259244f04dcd552413cad65e039a3a1
manuel <mdp.webdeveloper@gmail.com>
2015-08-04 20:11:09 +0200

    poner guapo el converter y exportar datos a la nueva tabla profesiones

--- excel/converter.php
+++ excel/converter.php
@@ -2,45 +2,43 @@
 
 //conectar
 require('../conexion.php');
-/*	
+	
 try{
-	$sql ="DROP TABLE IF EXISTS profesiones_sanitarias";
+	$sql ="DROP TABLE IF EXISTS profesiones";
 	if ($pdo->query($sql))
 		echo "<p>Tabla borrada correctamente.</p>\n";
-
-}catch(PDOException $Exception){
+} catch(PDOException $Exception) {
 	echo "<p>Error al borrar la tabla.<p>\n";
 	exit;
 }
 
 try {
-$sql="CREATE TABLE IF NOT EXISTS `profesiones_sanitarias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cod` int(11) NOT NULL,
-  `profesion` varchar(400) NOT NULL,
-  `descripcion` varchar(800) NOT NULL,
-  `estudios_asoc` varchar(400) NOT NULL,
-  `p_past` float NOT NULL,
-  `p_present` float NOT NULL,
-  `p_future` float NOT NULL,
-  `s_past` float NOT NULL,
-  `s_present` float NOT NULL,
-  `s_future` float NOT NULL,
-  `c_memoria` float NOT NULL,
-  `c_creatividad` float NOT NULL,
-  `c_comunicacion` float NOT NULL,
-  `c_forma_fisica` float NOT NULL,
-  `c_logica` float NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=92101051 ;
-" ;
-  if ($pdo->query($sql))
-  	print "<p>Tabla creada correctamente.</p>\n";
-
+	$sql="CREATE TABLE IF NOT EXISTS `profesiones` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`cod` int(11),
+	`profesion` varchar(100) NOT NULL,
+	`descripcion` varchar(500),
+	`estudios_asoc` varchar(500),
+	`p_past` float,
+	`p_present` float,
+	`p_future` float,
+	`s_past` float,
+	`s_present` float,
+	`s_future` float,
+	`c_memoria` float,
+	`c_creatividad` float,
+	`c_comunicacion` float,
+	`c_forma_fisica` float,
+	`c_logica` float,
+	`ultima_actualizacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`) ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=92101051 ;" ;
+	if ($pdo->query($sql))
+		print "<p>Tabla creada correctamente.</p>\n";
 } catch(PDOException $Exception) {
 	echo "<p>Error al crear la tabla.<p>\n";
 	exit;
-} */
+} 
+
 //usar PHPExcel_IOFactory
 include 'phpexcel-master/Classes/PHPExcel/IOFactory.php';
 //coger excel
@@ -61,12 +59,11 @@ $highestRow = $sheet->getHighestRow();
 $highestColumn = $sheet->getHighestColumn();
 
 //preparar sentencia INSERT
-//$sql .= "INSERT INTO profesiones_sanitarias VALUES (";
 //bucle de cada fila
 try{
 	$sql_insert="";
 	for ($row = 8; $row <= $highestRow; $row++) { 
-		$sql_insert.= "INSERT INTO `profesiones` ( `id` , `cod` , `profesion` , `descripcion` , `estudios_asoc` , `p_past` , `p_present` , `p_future` , `s_past` , `s_present` , `s_future` , `c_memoria` , `c_creatividad` , `c_comunicacion` , `c_forma_fisica` , `c_logica` ) VALUES (";
+		$sql_insert.= "INSERT INTO `profesiones` ( `cod` , `profesion` , `descripcion` , `estudios_asoc` , `p_past` , `p_present` , `p_future` , `s_past` , `s_present` , `s_future` , `c_memoria` , `c_creatividad` , `c_comunicacion` , `c_forma_fisica` , `c_logica` ) VALUES (";
 	    //poner los datos de cada fila en el array rowData
 	    $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
 	    //recoger datos del array rowData
@@ -89,14 +86,13 @@ try{
 		$c_logica = str_replace(',', '.', $rowData[0][14]);
 	   
 	    //insertar datos en el VALUE
-	    $sql_insert .= "'','$cod','$profesion','$descripcion','$estudios_asoc','$p_past','$p_present','$p_future','$s_past','$s_present','$s_future','$c_memoria','$c_creatividad','$c_comunicacion','$c_forma_fisica','$c_logica');";  
+	    $sql_insert .= "'$cod','$profesion','$descripcion','$estudios_asoc','$p_past','$p_present','$p_future','$s_past','$s_present','$s_future','$c_memoria','$c_creatividad','$c_comunicacion','$c_forma_fisica','$c_logica');";  
 	}
 	//cerrar sentencia INSERT
-
 	if ($pdo->query($sql_insert)) {
 	    print "<p>Registro creado correctamente.</p>\n";
-		echo "<table border='2px solid'><tr><td>id</td><td>cod</td><td>profesion</td><td>descripcion</td><td>estudios_asoc</td><td>p_past</td><td>p_present</td><td>p_future</td><td>s_past</td><td>s_present</td><td>s_future</td><td>c_memoria</td><td>c_creatividad</td><td>c_comunicacion</td><td>c_forma_fisica</td><td>c_logica</td></tr>";
-		$sql = 'SELECT * FROM profesiones_sanitarias';
+		echo "<table border='2px solid'><tr><td>id</td><td>cod</td><td>profesion</td><td>descripcion</td><td>estudios_asoc</td><td>p_past</td><td>p_present</td><td>p_future</td><td>s_past</td><td>s_present</td><td>s_future</td><td>c_memoria</td><td>c_creatividad</td><td>c_comunicacion</td><td>c_forma_fisica</td><td>c_logica</td><td>ultima_actualizacion</td></tr>";
+		$sql = 'SELECT * FROM profesiones';
 	    foreach ($pdo->query($sql) as $row) {
 			print "<td>".$row['id'] . "</td>";
 			print "<td>".$row['cod'] . "</td>";
@@ -113,7 +109,8 @@ try{
 			print "<td>".$row['c_creatividad'] . "</td>";
 			print "<td>".$row['c_comunicacion'] . "</td>";
 			print "<td>".$row['c_forma_fisica'] . "</td>";
-			print "<td>".$row['c_logica'] . "</td></tr>";   
+			print "<td>".$row['c_logica'] . "</td>";
+			print "<td>".$row['ultima_actualizacion'] . "</td></tr>";   
 	    }
 		echo "</table>";
 	} 
@@ -121,16 +118,8 @@ try{
 	echo "<p>Error al insertar los datos de la tabla.<p>\n";
 	exit;
 }
-$pdo = null;
 
-//preparar y ejecutar
-/*
-$statement = $pdo->prepare($sql);
-$statement->execute();
-if(!$statement){
-	echo "error al insertar registro";
-	}
-*/
+$pdo = null;
 
 ?>
 
