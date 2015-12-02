823d1e9687e474c83c6938eae40cfa29fa7b36c3
manuel <mdp.webdeveloper@gmail.com>
2015-12-02 23:05:39 +0100

    incluir capaciades en el generador de estaticas

--- generador_estaticas.php
+++ generador_estaticas.php
@@ -269,11 +269,11 @@ $html = '
 
   <script type="text/javascript" async>
     '; 
-    // puede que tenga que meter el script de highchart a pelo
-    
-$btn_colabora_s_1 = $btn_colabora_s_2 = 0;
+
+/**SALARIOS**/ 
+
+$btn_colabora_s_1 = 0;
 $s_junior_min = $s_junior_max = $s_intermedio_min = $s_intermedio_max = $s_senior_min = $s_senior_max = 0;
-$s_junior_min_dos = $s_junior_max_dos = $s_intermedio_min_dos = $s_intermedio_max_dos = $s_senior_min_dos = $s_senior_max_dos = 0;
 
 function imprimirSeriesSal($fila, $btn, $btn_colabora) {
     if( !is_null($fila) && !$fila == 0 )
@@ -284,14 +284,9 @@ function imprimirSeriesSal($fila, $btn, $btn_colabora) {
 
 foreach( $tablas['salarios'] as $n => $rango) {
     $$rango = imprimirSeriesSal($filas_salarios[0][$rango], $n, $btn_colabora_s_1);
-    if( isset($profesion_dos) && !empty($profesion_dos) ){
-        $rango_dos = $rango . '_dos';
-        $$rango_dos = imprimirSeriesSal($filas_salarios_dos[0][$rango], $n, $btn_colabora_s_2);
-    }
 }
 
-
-$script = 'var salarios = ['.
+$script_salarios = 'var salarios = ['.
     '[0, '. $s_junior_min .','. $s_junior_max .'],'.
     '[2, '. $s_junior_min .','. $s_junior_max .'],'.
     '[5, '. $s_intermedio_min .','. $s_intermedio_max .'],'. 
@@ -304,8 +299,7 @@ $script = 'var salarios = ['.
 '];';
 
 
-$script .= "$('#container_salarios').highcharts({
-
+$script_salarios .= "$('#container_salarios').highcharts({
     chart: {
         backgroundColor:'rgba(255, 255, 255, 0)',
         spacingBottom: 20,
@@ -315,29 +309,24 @@ $script .= "$('#container_salarios').highcharts({
         width: null,
         height: 380
     },
-
     title: {
         text: 'SALARIO',
         align: 'center'
     },
-
     legend: { 
         enable: false 
     },
-
     xAxis: {
         //categories: ['JUNIOR','INTERMEDIO','SENIOR']
         title: {
             text: 'EXPERIENCIA ' + '(años)'.toUpperCase()
         }
     },
-
     yAxis: {
         title: {
             text: 'SALARIO NETO (€/mes)'
         }
     },
-
     tooltip: {
         headerFormat: '<strong>{point.x} años de experiencia</strong><br>',
         //pointFormat: '{point.x} años de experiencia',
@@ -345,17 +334,16 @@ $script .= "$('#container_salarios').highcharts({
         shared: true,
         valueSuffix: ' €'
     },
-
     credits: {
         enabled: false
     },
-
+    colorBypoint: true,
+    colors: [ '#cc0000', '#cc0000' ],
     plotOptions: {
         arearange: {
             fillOpacity: 0.5
         }
     },
-
     series: [
         {
             name: '". $profesion ."',
@@ -379,8 +367,8 @@ $script .= "$('#container_salarios').highcharts({
 });";
 
 if( $btn_colabora_s_1 > 0 ) { 
-    $script .= "var capa_aviso = '<div class=\"capa-aviso\">';
-    capa_aviso += '<div class=\"cerrar-aviso\"><a href=\"#\"><img class=\"icon\" src=\"images/cross.svg\"></img></a></div>';
+    $script_salarios .= "var capa_aviso = '<div class=\"capa-aviso\">';
+    capa_aviso += '<div class=\"cerrar-aviso\"><a href=\"#\"><img class=\"icon\" src=\"../images/cross.svg\"></img></a></div>';
     capa_aviso += '<div class=\"col-md-10 col-md-offset-1\">';
     capa_aviso += '<h3>Aún no tenemos imformación suficiente!</h3>';
 
@@ -392,13 +380,159 @@ if( $btn_colabora_s_1 > 0 ) {
 
     $('#container_salarios').append(capa_aviso);";
 } 
+
+/**CAPACIDADES**/
+
+$descripciones = array(
+    'Análisis'                  => 'Razonamiento lógico, toma de decisiones, organización, gestión, etc.',
+    'Comunicación'              => 'Comunicación, hablar en público, escucha activa.',
+    'Capacidad física'          => 'Destreza técnica, apariciencia física, etc.',
+    'Cooperación'               => 'Empatía, sensibilidad, colaboración, trabajo en equipo, escucha.',
+    'Consecución de objetivos'  => 'Orientado a objetivos, resultados, etc.',
+    'Persuasión'                => 'Influencia, negociación, habilidades comerciales, etc.'
+);
+
+$btn_colabora_c_1 = 0;
+$c_analisis = $c_comunicacion = $c_equipo = $c_forma_fisica = $c_objetivos = $c_persuasion = 0;
+
+function imprimirSeriesCap($fila, $btn, $btn_colabora) {
+    if( !is_null($fila) && !$fila == 0 )
+        return $fila;
+    else
+        $btn_colabora = $btn + 1;
+}
+
+foreach( $tablas['capacidades'] as $n => $campo) {
+    $$campo = imprimirSeriesCap($filas_capacidades[0][$campo], $n, $btn_colabora_c_1);
+}
+
+$script_capacidades = 'var capacidades = ['.$c_analisis .','. $c_comunicacion .','. $c_equipo .','. $c_forma_fisica .','. $c_objetivos .','. $c_persuasion .'];';
+
+$script_capacidades .= "$('#container_capacidades').highcharts({
+    chart: {
+        polar: true,
+        type: 'line',
+        backgroundColor:'rgba(255, 255, 255, 0)',
+        // Edit chart size
+        spacingBottom: 20,
+        spacingTop: 20,
+        spacingLeft: 20,
+        spacingRight: 20,
+        width: null,
+        height: 380
+    },
+    title: {
+        text: 'CUALIDADES PROFESIONALES',
+        align: 'center'
+    },
+    legend: { enable: false },
+    pane: {
+        size: '80%'
+    },
+    xAxis: {
+        categories: [";
+       
+        foreach($descripciones as $nombre => $descripcion) { 
+            $script_capacidades .= '"'.$nombre.'",'; 
+        } 
+   
+        $script_capacidades .= "],
+        tickmarkPlacement: 'on',
+        lineWidth: 0,
+        gridLineColor: '#999999'
+    },
+    yAxis: {
+        gridLineInterpolation: 'polygon',
+        lineWidth: 0,
+        min: 0,
+        gridLineColor: '#999999'
+    },
+    exporting: {
+            buttons: {
+               anotherButton: {
+                    text: '?? No entiendo!',
+                    onclick: function () {
+                        var capa_glosario = '<div class=\"capa-glosario\">';
+                        capa_glosario += '<div class=\"cerrar-glosario\"><img class=\"icon\" src=\"../images/cross.svg\"></img></div>';
+                        capa_glosario += '<div class=\"col-md-10 col-md-offset-1\">';
+                       
+                        capa_glosario += '<h3>No te preocupes, te lo aclaramos aquí</h3>';
+                        capa_glosario += '<dl class=\"dl-horizontal\">';";
+                        foreach ($descripciones as $nombre => $descripcion) { 
+                            $script_capacidades .= "capa_glosario += '<dt>". $nombre .":</dt><dd>". $descripcion .":</dd>';";
+                        }
+                        $script_capacidades .= "capa_glosario += '</dl>';
+
+                        capa_glosario += '</div>';
+                        capa_glosario += '</div>';
+
+                        $('#container_capacidades').append(capa_glosario);
+
+                        // cerrar glosario
+                        $('.cerrar-glosario').click( function() {
+                            $(this).parent().remove();
+                        });
+                    }
+                }
+            }
+        },
+    tooltip: {
+        shared: true,
+        formatter: function() {
+            var descripciones = {
+                Analisis:                   '". $descripciones['Análisis'] ."',
+                Comunicacion:               '". $descripciones['Comunicación'] ."',
+                Capacidad_fisica:           '". $descripciones['Capacidad física'] ."',
+                Cooperacion:                '". $descripciones['Cooperación'] ."',
+                Consecucion_de_objetivos:   '". $descripciones['Consecución de objetivos'] ."',
+                Persuasion:                 '". $descripciones['Persuasión'] ."'
+            };
+
+            var Latinise={};Latinise.latin_map={'Á':'A','Ă':'A','Ắ':'A','Ặ':'A','Ằ':'A','Ẳ':'A','Ẵ':'A','Ǎ':'A','Â':'A','Ấ':'A','Ậ':'A','Ầ':'A','Ẩ':'A','Ẫ':'A','Ä':'A','Ǟ':'A','Ȧ':'A','Ǡ':'A','Ạ':'A','Ȁ':'A','À':'A','Ả':'A','Ȃ':'A','Ā':'A','Ą':'A','Å':'A','Ǻ':'A','Ḁ':'A','Ⱥ':'A','Ã':'A','Ꜳ':'AA','Æ':'AE','Ǽ':'AE','Ǣ':'AE','Ꜵ':'AO','Ꜷ':'AU','Ꜹ':'AV','Ꜻ':'AV','Ꜽ':'AY','Ḃ':'B','Ḅ':'B','Ɓ':'B','Ḇ':'B','Ƀ':'B','Ƃ':'B','Ć':'C','Č':'C','Ç':'C','Ḉ':'C','Ĉ':'C','Ċ':'C','Ƈ':'C','Ȼ':'C','Ď':'D','Ḑ':'D','Ḓ':'D','Ḋ':'D','Ḍ':'D','Ɗ':'D','Ḏ':'D','ǲ':'D','ǅ':'D','Đ':'D','Ƌ':'D','Ǳ':'DZ','Ǆ':'DZ','É':'E','Ĕ':'E','Ě':'E','Ȩ':'E','Ḝ':'E','Ê':'E','Ế':'E','Ệ':'E','Ề':'E','Ể':'E','Ễ':'E','Ḙ':'E','Ë':'E','Ė':'E','Ẹ':'E','Ȅ':'E','È':'E','Ẻ':'E','Ȇ':'E','Ē':'E','Ḗ':'E','Ḕ':'E','Ę':'E','Ɇ':'E','Ẽ':'E','Ḛ':'E','Ꝫ':'ET','Ḟ':'F','Ƒ':'F','Ǵ':'G','Ğ':'G','Ǧ':'G','Ģ':'G','Ĝ':'G','Ġ':'G','Ɠ':'G','Ḡ':'G','Ǥ':'G','Ḫ':'H','Ȟ':'H','Ḩ':'H','Ĥ':'H','Ⱨ':'H','Ḧ':'H','Ḣ':'H','Ḥ':'H','Ħ':'H','Í':'I','Ĭ':'I','Ǐ':'I','Î':'I','Ï':'I','Ḯ':'I','İ':'I','Ị':'I','Ȉ':'I','Ì':'I','Ỉ':'I','Ȋ':'I','Ī':'I','Į':'I','Ɨ':'I','Ĩ':'I','Ḭ':'I','Ꝺ':'D','Ꝼ':'F','Ᵹ':'G','Ꞃ':'R','Ꞅ':'S','Ꞇ':'T','Ꝭ':'IS','Ĵ':'J','Ɉ':'J','Ḱ':'K','Ǩ':'K','Ķ':'K','Ⱪ':'K','Ꝃ':'K','Ḳ':'K','Ƙ':'K','Ḵ':'K','Ꝁ':'K','Ꝅ':'K','Ĺ':'L','Ƚ':'L','Ľ':'L','Ļ':'L','Ḽ':'L','Ḷ':'L','Ḹ':'L','Ⱡ':'L','Ꝉ':'L','Ḻ':'L','Ŀ':'L','Ɫ':'L','ǈ':'L','Ł':'L','Ǉ':'LJ','Ḿ':'M','Ṁ':'M','Ṃ':'M','Ɱ':'M','Ń':'N','Ň':'N','Ņ':'N','Ṋ':'N','Ṅ':'N','Ṇ':'N','Ǹ':'N','Ɲ':'N','Ṉ':'N','Ƞ':'N','ǋ':'N','Ñ':'N','Ǌ':'NJ','Ó':'O','Ŏ':'O','Ǒ':'O','Ô':'O','Ố':'O','Ộ':'O','Ồ':'O','Ổ':'O','Ỗ':'O','Ö':'O','Ȫ':'O','Ȯ':'O','Ȱ':'O','Ọ':'O','Ő':'O','Ȍ':'O','Ò':'O','Ỏ':'O','Ơ':'O','Ớ':'O','Ợ':'O','Ờ':'O','Ở':'O','Ỡ':'O','Ȏ':'O','Ꝋ':'O','Ꝍ':'O','Ō':'O','Ṓ':'O','Ṑ':'O','Ɵ':'O','Ǫ':'O','Ǭ':'O','Ø':'O','Ǿ':'O','Õ':'O','Ṍ':'O','Ṏ':'O','Ȭ':'O','Ƣ':'OI','Ꝏ':'OO','Ɛ':'E','Ɔ':'O','Ȣ':'OU','Ṕ':'P','Ṗ':'P','Ꝓ':'P','Ƥ':'P','Ꝕ':'P','Ᵽ':'P','Ꝑ':'P','Ꝙ':'Q','Ꝗ':'Q','Ŕ':'R','Ř':'R','Ŗ':'R','Ṙ':'R','Ṛ':'R','Ṝ':'R','Ȑ':'R','Ȓ':'R','Ṟ':'R','Ɍ':'R','Ɽ':'R','Ꜿ':'C','Ǝ':'E','Ś':'S','Ṥ':'S','Š':'S','Ṧ':'S','Ş':'S','Ŝ':'S','Ș':'S','Ṡ':'S','Ṣ':'S','Ṩ':'S','Ť':'T','Ţ':'T','Ṱ':'T','Ț':'T','Ⱦ':'T','Ṫ':'T','Ṭ':'T','Ƭ':'T','Ṯ':'T','Ʈ':'T','Ŧ':'T','Ɐ':'A','Ꞁ':'L','Ɯ':'M','Ʌ':'V','Ꜩ':'TZ','Ú':'U','Ŭ':'U','Ǔ':'U','Û':'U','Ṷ':'U','Ü':'U','Ǘ':'U','Ǚ':'U','Ǜ':'U','Ǖ':'U','Ṳ':'U','Ụ':'U','Ű':'U','Ȕ':'U','Ù':'U','Ủ':'U','Ư':'U','Ứ':'U','Ự':'U','Ừ':'U','Ử':'U','Ữ':'U','Ȗ':'U','Ū':'U','Ṻ':'U','Ų':'U','Ů':'U','Ũ':'U','Ṹ':'U','Ṵ':'U','Ꝟ':'V','Ṿ':'V','Ʋ':'V','Ṽ':'V','Ꝡ':'VY','Ẃ':'W','Ŵ':'W','Ẅ':'W','Ẇ':'W','Ẉ':'W','Ẁ':'W','Ⱳ':'W','Ẍ':'X','Ẋ':'X','Ý':'Y','Ŷ':'Y','Ÿ':'Y','Ẏ':'Y','Ỵ':'Y','Ỳ':'Y','Ƴ':'Y','Ỷ':'Y','Ỿ':'Y','Ȳ':'Y','Ɏ':'Y','Ỹ':'Y','Ź':'Z','Ž':'Z','Ẑ':'Z','Ⱬ':'Z','Ż':'Z','Ẓ':'Z','Ȥ':'Z','Ẕ':'Z','Ƶ':'Z','Ĳ':'IJ','Œ':'OE','ᴀ':'A','ᴁ':'AE','ʙ':'B','ᴃ':'B','ᴄ':'C','ᴅ':'D','ᴇ':'E','ꜰ':'F','ɢ':'G','ʛ':'G','ʜ':'H','ɪ':'I','ʁ':'R','ᴊ':'J','ᴋ':'K','ʟ':'L','ᴌ':'L','ᴍ':'M','ɴ':'N','ᴏ':'O','ɶ':'OE','ᴐ':'O','ᴕ':'OU','ᴘ':'P','ʀ':'R','ᴎ':'N','ᴙ':'R','ꜱ':'S','ᴛ':'T','ⱻ':'E','ᴚ':'R','ᴜ':'U','ᴠ':'V','ᴡ':'W','ʏ':'Y','ᴢ':'Z','á':'a','ă':'a','ắ':'a','ặ':'a','ằ':'a','ẳ':'a','ẵ':'a','ǎ':'a','â':'a','ấ':'a','ậ':'a','ầ':'a','ẩ':'a','ẫ':'a','ä':'a','ǟ':'a','ȧ':'a','ǡ':'a','ạ':'a','ȁ':'a','à':'a','ả':'a','ȃ':'a','ā':'a','ą':'a','ᶏ':'a','ẚ':'a','å':'a','ǻ':'a','ḁ':'a','ⱥ':'a','ã':'a','ꜳ':'aa','æ':'ae','ǽ':'ae','ǣ':'ae','ꜵ':'ao','ꜷ':'au','ꜹ':'av','ꜻ':'av','ꜽ':'ay','ḃ':'b','ḅ':'b','ɓ':'b','ḇ':'b','ᵬ':'b','ᶀ':'b','ƀ':'b','ƃ':'b','ɵ':'o','ć':'c','č':'c','ç':'c','ḉ':'c','ĉ':'c','ɕ':'c','ċ':'c','ƈ':'c','ȼ':'c','ď':'d','ḑ':'d','ḓ':'d','ȡ':'d','ḋ':'d','ḍ':'d','ɗ':'d','ᶑ':'d','ḏ':'d','ᵭ':'d','ᶁ':'d','đ':'d','ɖ':'d','ƌ':'d','ı':'i','ȷ':'j','ɟ':'j','ʄ':'j','ǳ':'dz','ǆ':'dz','é':'e','ĕ':'e','ě':'e','ȩ':'e','ḝ':'e','ê':'e','ế':'e','ệ':'e','ề':'e','ể':'e','ễ':'e','ḙ':'e','ë':'e','ė':'e','ẹ':'e','ȅ':'e','è':'e','ẻ':'e','ȇ':'e','ē':'e','ḗ':'e','ḕ':'e','ⱸ':'e','ę':'e','ᶒ':'e','ɇ':'e','ẽ':'e','ḛ':'e','ꝫ':'et','ḟ':'f','ƒ':'f','ᵮ':'f','ᶂ':'f','ǵ':'g','ğ':'g','ǧ':'g','ģ':'g','ĝ':'g','ġ':'g','ɠ':'g','ḡ':'g','ᶃ':'g','ǥ':'g','ḫ':'h','ȟ':'h','ḩ':'h','ĥ':'h','ⱨ':'h','ḧ':'h','ḣ':'h','ḥ':'h','ɦ':'h','ẖ':'h','ħ':'h','ƕ':'hv','í':'i','ĭ':'i','ǐ':'i','î':'i','ï':'i','ḯ':'i','ị':'i','ȉ':'i','ì':'i','ỉ':'i','ȋ':'i','ī':'i','į':'i','ᶖ':'i','ɨ':'i','ĩ':'i','ḭ':'i','ꝺ':'d','ꝼ':'f','ᵹ':'g','ꞃ':'r','ꞅ':'s','ꞇ':'t','ꝭ':'is','ǰ':'j','ĵ':'j','ʝ':'j','ɉ':'j','ḱ':'k','ǩ':'k','ķ':'k','ⱪ':'k','ꝃ':'k','ḳ':'k','ƙ':'k','ḵ':'k','ᶄ':'k','ꝁ':'k','ꝅ':'k','ĺ':'l','ƚ':'l','ɬ':'l','ľ':'l','ļ':'l','ḽ':'l','ȴ':'l','ḷ':'l','ḹ':'l','ⱡ':'l','ꝉ':'l','ḻ':'l','ŀ':'l','ɫ':'l','ᶅ':'l','ɭ':'l','ł':'l','ǉ':'lj','ſ':'s','ẜ':'s','ẛ':'s','ẝ':'s','ḿ':'m','ṁ':'m','ṃ':'m','ɱ':'m','ᵯ':'m','ᶆ':'m','ń':'n','ň':'n','ņ':'n','ṋ':'n','ȵ':'n','ṅ':'n','ṇ':'n','ǹ':'n','ɲ':'n','ṉ':'n','ƞ':'n','ᵰ':'n','ᶇ':'n','ɳ':'n','ñ':'n','ǌ':'nj','ó':'o','ŏ':'o','ǒ':'o','ô':'o','ố':'o','ộ':'o','ồ':'o','ổ':'o','ỗ':'o','ö':'o','ȫ':'o','ȯ':'o','ȱ':'o','ọ':'o','ő':'o','ȍ':'o','ò':'o','ỏ':'o','ơ':'o','ớ':'o','ợ':'o','ờ':'o','ở':'o','ỡ':'o','ȏ':'o','ꝋ':'o','ꝍ':'o','ⱺ':'o','ō':'o','ṓ':'o','ṑ':'o','ǫ':'o','ǭ':'o','ø':'o','ǿ':'o','õ':'o','ṍ':'o','ṏ':'o','ȭ':'o','ƣ':'oi','ꝏ':'oo','ɛ':'e','ᶓ':'e','ɔ':'o','ᶗ':'o','ȣ':'ou','ṕ':'p','ṗ':'p','ꝓ':'p','ƥ':'p','ᵱ':'p','ᶈ':'p','ꝕ':'p','ᵽ':'p','ꝑ':'p','ꝙ':'q','ʠ':'q','ɋ':'q','ꝗ':'q','ŕ':'r','ř':'r','ŗ':'r','ṙ':'r','ṛ':'r','ṝ':'r','ȑ':'r','ɾ':'r','ᵳ':'r','ȓ':'r','ṟ':'r','ɼ':'r','ᵲ':'r','ᶉ':'r','ɍ':'r','ɽ':'r','ↄ':'c','ꜿ':'c','ɘ':'e','ɿ':'r','ś':'s','ṥ':'s','š':'s','ṧ':'s','ş':'s','ŝ':'s','ș':'s','ṡ':'s','ṣ':'s','ṩ':'s','ʂ':'s','ᵴ':'s','ᶊ':'s','ȿ':'s','ɡ':'g','ᴑ':'o','ᴓ':'o','ᴝ':'u','ť':'t','ţ':'t','ṱ':'t','ț':'t','ȶ':'t','ẗ':'t','ⱦ':'t','ṫ':'t','ṭ':'t','ƭ':'t','ṯ':'t','ᵵ':'t','ƫ':'t','ʈ':'t','ŧ':'t','ᵺ':'th','ɐ':'a','ᴂ':'ae','ǝ':'e','ᵷ':'g','ɥ':'h','ʮ':'h','ʯ':'h','ᴉ':'i','ʞ':'k','ꞁ':'l','ɯ':'m','ɰ':'m','ᴔ':'oe','ɹ':'r','ɻ':'r','ɺ':'r','ⱹ':'r','ʇ':'t','ʌ':'v','ʍ':'w','ʎ':'y','ꜩ':'tz','ú':'u','ŭ':'u','ǔ':'u','û':'u','ṷ':'u','ü':'u','ǘ':'u','ǚ':'u','ǜ':'u','ǖ':'u','ṳ':'u','ụ':'u','ű':'u','ȕ':'u','ù':'u','ủ':'u','ư':'u','ứ':'u','ự':'u','ừ':'u','ử':'u','ữ':'u','ȗ':'u','ū':'u','ṻ':'u','ų':'u','ᶙ':'u','ů':'u','ũ':'u','ṹ':'u','ṵ':'u','ᵫ':'ue','ꝸ':'um','ⱴ':'v','ꝟ':'v','ṿ':'v','ʋ':'v','ᶌ':'v','ⱱ':'v','ṽ':'v','ꝡ':'vy','ẃ':'w','ŵ':'w','ẅ':'w','ẇ':'w','ẉ':'w','ẁ':'w','ⱳ':'w','ẘ':'w','ẍ':'x','ẋ':'x','ᶍ':'x','ý':'y','ŷ':'y','ÿ':'y','ẏ':'y','ỵ':'y','ỳ':'y','ƴ':'y','ỷ':'y','ỿ':'y','ȳ':'y','ẙ':'y','ɏ':'y','ỹ':'y','ź':'z','ž':'z','ẑ':'z','ʑ':'z','ⱬ':'z','ż':'z','ẓ':'z','ȥ':'z','ẕ':'z','ᵶ':'z','ᶎ':'z','ʐ':'z','ƶ':'z','ɀ':'z','ﬀ':'ff','ﬃ':'ffi','ﬄ':'ffl','ﬁ':'fi','ﬂ':'fl','ĳ':'ij','œ':'oe','ﬆ':'st','ₐ':'a','ₑ':'e','ᵢ':'i','ⱼ':'j','ₒ':'o','ᵣ':'r','ᵤ':'u','ᵥ':'v','ₓ':'x'};
+            String.prototype.latinise=function(){return this.replace(/[^A-Za-z0-9\[\] ]/g,function(a){return Latinise.latin_map[a]||a})};
+            String.prototype.latinize=String.prototype.latinise;
+            String.prototype.isLatin=function(){return this==this.latinise()}
+            
+            return '<strong>'+ descripciones[this.x.replace(/ /g,'_').latinize()] +'</strong><br/>'+
+            '<span style=\"color:'+this.points[0].series.color+'\">'+this.points[0].series.name+': <strong>'+this.points[0].y+'</strong><br/>';
+        },
+        headerFormat: '<strong>{point.key}</strong><br>'      
+    },
+    credits: {
+         enabled: false
+    },
+
+    series: [{  
+        name: '". mb_strtoupper($profesion,"UTF-8") ."',
+        data: capacidades,
+        stack: '". $profesion ."'
+    }]
+});";
+
+if( $btn_colabora_c_1 > 0 ) { 
+    
+    $script_capacidades .= "var capa_aviso = '<div class=\"capa-aviso\">';
+    capa_aviso += '<div class=\"cerrar-aviso\"><a href=\"#\"><img class=\"icon\" src=\"../images/cross.svg\"></img></a></div>';
+    capa_aviso += '<div class=\"col-md-10 col-md-offset-1\">';
+    capa_aviso += '<h3>Aún no tenemos imformación suficiente!</h3>';
+
+        capa_aviso += '<p class=\"text-center\">Ayúdanos a completar información sobre <strong>cualidades profesionales</strong> de la profesión<br>';
+        capa_aviso += '<strong>". mb_strtoupper($profesion,"UTF-8") ."</strong></p>';
+        capa_aviso += '<a href=\"colabora.php?profesion=". $profesion ."\" class=\"btn btn-aviso\" style=\"border-color: rgb(204, 0, 0); color: rgb(204, 0, 0);\">Colabora!</a>';
+
+    capa_aviso += '</div>';
+    capa_aviso += '</div>';
+
+    $('#container_capacidades').append(capa_aviso);";
+}
+
       /*$html .=*/  //include('../js/grafica_salarios.js');
       //$html .= include("js/grafica_capacidades.js");
       /*$html .= '"' .*/ //include('../js/grafica_empleabilidad.js'); 
       /*$html .= '"' .*/ //include('../js/grafica_formacion.js');
       //$script = include('js/grafica_satisfaccion.js');
       /*$html .= '"' .*/ //include('../js/grafica_info.js'); 
-    $html .= $script.'
+    $html .= $script_salarios . $script_capacidades .'
   </script>
 </html>';
     echo $script;
