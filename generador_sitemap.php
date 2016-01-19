c1c282af43b161d2d3811b5ad77c216d5e049ea4
manuel <mdp.webdeveloper@gmail.com>
2016-01-19 21:49:19 +0100

    preparar generador para servidor

--- generador_sitemap.php
+++ generador_sitemap.php
@@ -13,8 +13,8 @@ $sitemap = new Sitemap;
 //Add items to sitemap:
 //Loop de todas las url creadas
 foreach(glob('profesiones/*.html') as $filename) {
-	//TEST//$url = 'http://' . $_SERVER['HTTP_HOST'] . '/' . $filename;
-	$url = 'http://' . $_SERVER['HTTP_HOST'] . '/queserademi/queserademi/' . $filename;
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . '/' . $filename;
+	//TEST//$url = 'http://' . $_SERVER['HTTP_HOST'] . '/queserademi/queserademi/' . $filename;
 	echo $url.'<br>';
 	$sitemap->addItem($url);
 	$sitemap->addItem($url, '0.7', ChangeFreq::MONTHLY, new \DateTime()); //You can add some optional params
