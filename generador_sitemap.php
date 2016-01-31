9df14772f8706adc3a72558182a3bdf6aa4ad010
manuel <mdp.webdeveloper@gmail.com>
2016-01-31 14:39:42 +0100

    generar mas sitemaps con menos profesiones

--- generador_sitemap.php
+++ generador_sitemap.php
@@ -5,31 +5,35 @@ set_time_limit(0);
 // Include Composer autoloader
 include 'vendor/autoload.php';
 
-//Create a sitemap object
 use Asika\Sitemap\Sitemap;
 use Asika\Sitemap\ChangeFreq;
-$sitemap = new Sitemap;
 
+//Create a sitemap object
+$sitemap = new Sitemap;
 //Add items to sitemap:
 //Loop de todas las url creadas
-foreach(glob('profesiones/*.html') as $filename) {
+$limite = 1506;
+$num = 1;
+foreach(glob('profesiones/*.html') as $n => $filename) {
 	$url = 'http://' . $_SERVER['HTTP_HOST'] . '/' . $filename;
 	if ($_SERVER['HTTP_HOST'] === 'localhost')
 		$url = 'http://' . $_SERVER['HTTP_HOST'] . '/queserademi/queserademi/' . $filename;
-	echo $url.'<br>';
 	$sitemap->addItem($url);
 	$sitemap->addItem($url, '0.7', ChangeFreq::MONTHLY, new \DateTime()); //You can add some optional params
+	if ($n % $limite === 0 && $n > 0) {
+		//Render it to XML:
+		header('Content-Type: application/xml');
+		$nombre_sitemap = 'sitemap-' . $num . '.xml';
+		$pagina_xml = fopen($nombre_sitemap, "w+") or die("No se puede crear este sitemap");
+		// guardar xml
+		fwrite($pagina_xml, $sitemap);
+		fclose($pagina_xml);
+		echo $sitemap->toString();
+		// vaciar sitemap - nueva instancia
+		$sitemap = new Sitemap;
+		$num++;
+	}
 }
-
-//Render it to XML:
-header('Content-Type: application/xml');
-
-echo $sitemap->toString();
-
-$pagina_xml = fopen('sitemap.xml', "w+") or die("No se puede crear este documento");
-// guardar xml
-fwrite($pagina_xml, $sitemap);
-fclose($pagina_xml);
 
 exit();
 ?>
