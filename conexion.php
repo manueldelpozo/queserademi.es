00f6c6207df8d57c55c8bd6f0f015365cc0cc1a6
manuel <mdp.webdeveloper@gmail.com>
2017-01-30 21:59:06 +0100

    nueva integracion

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
 
 function connect() {
-	$host = "qxc430.queserademi.com";
-	$database = "qxc430";
-	$user = "qxc430";
-	$password = "Qsdm2017";
+	$host = "qth809.queserademi.es";
+	$database = "qth809";
+	$user = "qth809";
+	$password = "Qsdm2015";
 	$arrayPDO = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-    return new PDO( 'mysql:host='.$host.';dbname='.$database, $user, $password, $arrayPDO );
+    return new PDO('mysql:host='.$host.';dbname='.$database, $user, $password, $arrayPDO);
 }
 $pdo = connect();
 
