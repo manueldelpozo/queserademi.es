6dbe81769ba561eb0de75807c5a3f31aa49e8c68
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2017-03-13 22:43:57 +0100

    cambios del tema de 2016 de wp

--- noticias/wp-content/themes/twentysixteen/index.php
+++ noticias/wp-content/themes/twentysixteen/index.php
@@ -1,62 +1,31 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<div id="blog">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <section>
+        <?php define( 'WP_USE_THEMES', false ); get_header(); ?>
 
-		<?php if ( have_posts() ) : ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+          <article class="blogbox">
+                        <div class="blogimg"><a href="<?php the_permalink();?>">
+                            <?php if (has_post_thumbnail() ) {the_post_thumbnail('list_articles_thumbs');}?>
+                        </a></div>
+                        <div class="blogtitle"><a href="<?php the_permalink();?>"><?php the_title();?></a></div>
+                        <div class="gallerytext">
+                          <p><?php the_excerpt();?></p>
+                        </div>
+                        <div class="blogdata">
+                            <p><?php the_date();?></p>
+                        </div>
+          </article>
 
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
+        <?php endwhile; else : ?>
+        <h1>No se encontraron Articulos</h1>
+        <?php endif; ?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+  </section>
 
-			// End the loop.
-			endwhile;
+</div>
 
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-			) );
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- noticias/wp-content/themes/twentysixteen/single.php
+++ noticias/wp-content/themes/twentysixteen/single.php
@@ -1,54 +1,29 @@
-<?php
-/**
- * The template for displaying all single posts and attachments
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<div id="blog">
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+  <section>
+        <?php define( 'WP_USE_THEMES', false ); get_header(); ?>
 
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
+          <article class="blogbox2">
+                        <div class="blogtitle2"><a><?php the_title();?></a></div>
+                        <div class="blogdata">
+                            <p><?php the_date();?></p>
+                        </div>
+                        <div class="gallerytext2">
+                          <p><?php the_content();?></p>
+                        </div>
 
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
-				) );
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-			}
+          </article>
 
-			// End of the loop.
-		endwhile;
-		?>
+        <?php endwhile; else : ?>
+        <h1>No se encontraron Articulos</h1>
+        <?php endif; ?>
 
-	</main><!-- .site-main -->
+  </section>
 
-	<?php get_sidebar( 'content-bottom' ); ?>
+</div>
 
-</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
