f9f0b7cbaa73e3ecf56fc5b2134281691fc7f26e
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2017-03-16 23:19:08 +0100

    unificar estilos del blog de noticias

--- noticias/wp-content/themes/twentysixteen/header.php
+++ noticias/wp-content/themes/twentysixteen/header.php
@@ -21,9 +21,32 @@
 	    <meta property="og:image" content="http://queserademi.com/images/logo.png">
 	    <!--Links css-->
 	    <link rel="icon" type="image/x-icon" href="http://queserademi.com/images/logo.png">
+<link rel="stylesheet" href="http://queserademi.com/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css">
+	    <link rel="stylesheet" href="http://queserademi.com/css/style.css">
 	    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>">
-        <link href="https://fonts.googleapis.com/css?family=Muli:200,700" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css?family=Muli:200,700" rel="stylesheet">
+	    <script type="text/javascript" src="http://queserademi.com/js/jquery-2.1.3.js"></script>
+            <script type="text/javascript" src="http://queserademi.com/js/bootstrap.min.js"></script>
+	    <script type="text/javascript" src="http://queserademi.com/js/scripts.js"></script>
     </head>
 
   <header>
+<div class="row header">
+
+	            <div class="col-md-3 col-md-offset-1 col-xs-11 col-xs-offset-1">  
+	              <h4>Conoce todas las novedades en <br><strong>queserademi</strong></h4>  
+	            </div>
+
+	            <div class="col-md-4 hidden-xs text-center">
+	              <a href="http://queserademi.com">
+	                <img class="img-responsive" src="http://queserademi.com/images/logo.svg" height="60px"> 
+	              </a>
+	            </div>
+
+	            <div class="col-md-3 hidden-xs" align="right">
+	              <h4><a href="http://queserademi.com/colabora.php">Colabora, son solo 2 min!</a></h4>
+	            </div>
+
+	        </div>
   </header>
