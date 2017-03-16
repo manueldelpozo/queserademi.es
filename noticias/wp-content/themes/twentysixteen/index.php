1dda5cce7b8329506d75bd1978a4743db04a9167
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2017-03-16 23:29:33 +0100

    mover bg al header del blog para que salga en las paginas

--- noticias/wp-content/themes/twentysixteen/index.php
+++ noticias/wp-content/themes/twentysixteen/index.php
@@ -1,18 +1,5 @@
 <?php get_header(); ?>
-<!-- Google Tag Manager -->
-		<noscript>
-			<iframe src="//www.googletagmanager.com/ns.html?id=GTM-WS6V49" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-		</noscript>
-		<script>
-			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			})(window,document,'script','dataLayer','GTM-WS6V49');
-		</script>
-        <!-- End Google Tag Manager -->
-        <div id="preloader"></div>
-<div class="background-image"></div>
+
 <div id="blog">
 
   <section>
