5f68c72c0624afc01f91cb924664c64aa5846086
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2017-03-23 22:23:36 +0100

    actualizar tag manager

--- colabora.php
+++ colabora.php
@@ -33,20 +33,23 @@ if( isset( $_POST['profesion']  )  )
     <link rel="stylesheet" href="js/jquery.mobile-1.4.5/jquery.mobile-1.4.5.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style-colabora.css">
-  </head>
-  <body>
     <!-- Google Tag Manager -->
-    <noscript>
-      <iframe src="//www.googletagmanager.com/ns.html?id=GTM-WS6V49" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    </noscript>
     <script>
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-WS6V49');
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-KSJZX5B');
     </script>
     <!-- End Google Tag Manager -->
+  </head>
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KSJZX5B"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="preloader"></div>
     <div class="background-image grayscale blur"></div>
     <div data-role="page">
