b17c91344f8a3c2f797b68202df2eb6b050f4fc7
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2017-11-03 23:05:48 +0100

    change browser header color

--- comparador.php
+++ comparador.php
@@ -109,7 +109,7 @@ set_time_limit(0);
     <meta name="robots" content="noodp">
     <meta name="viewport" content="width=device-width, initial-scale = 1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#c00">
+    <meta name="theme-color" content="#d62e46">
     <!--OGs-->
     <link rel="canonical" href="https://queserademi.com/comparador.php">
     <meta property="og:locale" content="es_ES">
--- verificador.php
+++ verificador.php
@@ -265,7 +265,7 @@ if( !empty( $_POST['verificacion'] ) ){
 	    <meta name="robots" content="noodp">
 	    <meta name="viewport" content="width=device-width, initial-scale = 1.0">
 	    <meta name="apple-mobile-web-app-capable" content="yes">
-	    <meta name="theme-color" content="#c00">
+	    <meta name="theme-color" content="#d62e46">
 	    <!--OGs-->
 	    <link rel="canonical" href="http://queserademi.com/verificador.php">
 	    <meta property="og:locale" content="es_ES">
